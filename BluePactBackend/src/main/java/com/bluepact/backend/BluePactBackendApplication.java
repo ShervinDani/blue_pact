b67c2edd98d342a245d3145f93f1d82dc01e7648
ShervinDani <rvshervin@gmil.com>
2025-06-23 15:17:03 +0530

    created blueprint

---
+++ BluePactBackend/src/main/java/com/bluepact/backend/BluePactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bluepact.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluePactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluePactBackendApplication.class, args);
+	}
+
+}
---
+++ BluePactBackend/src/test/java/com/bluepact/backend/BluePactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluepact.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluePactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
